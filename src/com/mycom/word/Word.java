d03d19ef1d2cc4ab32b096b62ddd194641a5b7c4
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 <apple0605a@gmail.com>
2023-09-08 02:37:49 +0900

    toString 재정의 및 listAll 함수 생성

--- src/com/mycom/word/Word.java
+++ src/com/mycom/word/Word.java
@@ -1,6 +1,18 @@
 package com.mycom.word;
 
 public class Word {
+	
+	@Override	//우클릭 - source - override - toString
+	public String toString() {
+		
+		String slevel = "";		//별 받을 문자열, 별 자리에 이거 출력
+		for (int i = 0; i < level; ++i) slevel += "*";	//난이도만큼 별 개수 추가
+		String str = String.format("%-3s", slevel)		//좌측정렬, 별 개수 최대 세개
+			+ String.format("%15s", word)				//우측정렬
+			+ " " + meaning;							//뜻은 정렬 할 필요 無
+		return str;
+	}	//wordCRUD에서 요거 씀
+	
 	private int id;
 	private int level;
 	private String word;
